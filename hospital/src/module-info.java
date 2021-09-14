7c6c137c126158d638511d20a53dff0de1ad36d0
Guilherme-Macanan <83425581+Guilherme-Macanan@users.noreply.github.com>
2021-09-14 13:57:50 -0300

    Add files via upload

---
+++ hospital/src/module-info.java
@@ -0,0 +1,19 @@
+module hospital {
+
+public class Principaçl {
+	
+	public static void main(String[] args) {
+		Endereco end1 = new Endereco();
+		end1.setRua("Tv. Não te interessa");
+		end1.setCidade("Porto Alegre");
+		end1.setBairro("Centro");
+		end1.setCEP("000");
+		
+		Medico med1 = new Medico();
+		med1.enderco.getRua());
+		
+		System.out.println("o médico mora na rua"
+")
+	}
+}
+}
