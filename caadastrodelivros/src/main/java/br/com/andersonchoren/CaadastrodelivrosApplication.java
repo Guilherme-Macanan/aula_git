22bbf1301e64370d88b489362fd4dd76cf2dcde4
Guilherme-Macanan <83425581+Guilherme-Macanan@users.noreply.github.com>
2021-10-20 14:35:23 -0300

    Add files via upload

---
+++ caadastrodelivros/src/main/java/br/com/andersonchoren/CaadastrodelivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.andersonchoren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaadastrodelivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaadastrodelivrosApplication.class, args);
+	}
+
+}
